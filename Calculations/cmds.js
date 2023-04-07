5e66ba91c5a43117498f496e95cac82c703d531e
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-07 19:39:06 +0530

    Mathematics

---
+++ Calculations/cmds.js
@@ -0,0 +1,30 @@
+function add() {
+    var num1 = parseInt(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseInt(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseInt(num1 + num2); //Calculations
+    document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
+}
+function sub() {
+    var num1 = parseInt(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseInt(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseInt(num1 - num2); //Calculations
+    document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
+}
+function mult() {
+    var num1 = parseInt(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseInt(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseInt(num1 * num2); //Calculations
+    document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
+}
+function div() {
+    var num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseFloat(num1 / num2); //Calculations
+    document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
+}
+function modu() {
+    var num1 = parseInt(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseInt(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseInt(num1 % num2); //Calculations
+    document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
+}
