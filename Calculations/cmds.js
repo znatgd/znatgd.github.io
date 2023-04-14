4ae923f26644f95cedbebc4eee642244d3b0b40c
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-14 20:56:46 +0530

    should work

--- Calculations/cmds.js
+++ Calculations/cmds.js
@@ -1,39 +1,39 @@
 function add() {
-    let num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
-    let num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
-    let sum = parseInt(num1 + num2); //Calculations
+    var num1 = parseFloat(prompt("Enter parseFloatvarfirst number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = num1 + num2; //Calculations
     document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
 }
 function sub() {
-    let num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
-    let num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
-    let sum = parseInt(num1 - num2); //Calculations
+    var num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = num1 - num2; //Calculations
     document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
 }
 
 function mult() {
-    let num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
-    let num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
-    let sum = parseInt(num1 * num2); //Calculations
+    var num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = num1 * num2; //Calculations
     document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
     }
 
 function divi() {
-    let num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
-    let num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
-    let sum = parseInt(num1 / num2); //Calculations
+    var num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = num1 / num2; //Calculations
     document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
 }
 
 function modu() {
-    let num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
-    let num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
-    let sum = parseInt(num1 % num2); //Calculations
+    var num1 = parseFloat(prompt("Enter first number: ", "13")); //Input answer and converts from string to int
+    var num2 = parseFloat(prompt("Enter the second number: ", "25")); //Input answer and converts from string to int
+    var sum = parseFloat(num1 % num2); //Calculations
     document.getElementById('output').innerHTML = "Your answer is " + sum; //Output
 }
 
 function sqr() {
-    let num = parseFloat(prompt("Enter the number to square:", "")); //Input answer
-    let sum = num * num; //Square
+    var num = parseFloat(prompt("Enter the number to square:", "")); //Input answer
+    var sum = num * num; //Square
     document.getElementByID('output').innerHTML = "Your answer is " + sum; //Output 
 }
