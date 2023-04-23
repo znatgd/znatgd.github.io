ed9797852bcad58e64f848fff7dcd6ccd810af95
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-23 19:29:39 +0530

    moar things committed

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("MKMusic website");
+var userID = 1;
+while (userID!=5) {
+    userID++;
+}
+function loginID() {
+    var username = prompt("Enter your username", "example");
+    if (username==null) {
+        alert("Please enter a username");
+    }
+    if (username==false) {
+        alert("Exiting manager...");
+    }
+    if (username==true) {
+        document.getElementById('message').innerHTML = "Welcome " + username;
+        if (username=="ADMINPASS") {
+            userID = 1;
+            document.getElementById('message').innerHTML = "Welcome " + username + "#" + userID;
+        }
+    }
+}
+//please do not change this...
