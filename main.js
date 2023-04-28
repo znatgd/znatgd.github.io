bdee1997dbe1214d2f6f5ecf1508985ddae69864
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-28 18:37:51 +0530

    tiny.c

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ function loginID() {
     var username = prompt("Enter your username", "example");
     if (username==null) {
         alert("Please enter a username");
+        return 0;
     }
     if (username==false) {
         alert("Exiting manager...");
+        return 0;
     }
     if (username==true) {
         document.getElementByID('message').innerHTML = "Welcome " + username;
+        return 0;
         if (username=="ADMINPASS") {
             userID = 1;
-            document.getElementByID('message').innerHTML = "Welcome " + username + "#" + userID;
+            document.getElementByID("message").innerHTML = "Welcome " + username + "#" + userID;
+            return 0;
         }
     }
 }
