367bcd6036db5da77ab68c9a7a57532ef14c0b1a
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-28 18:44:59 +0530

    this is the last attempt on username

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 var userID = 1;
-while (userID!=5) {
-    userID++;
-}
 function loginID() {
     var username = prompt("Enter your username", "example");
-    document.getElementByID('message').innerHTML = "Welcome " + username;
+    document.getElementByID("greeting").innerHTML = "Welcome " + username;
     if (username=="ADMINPASS") {
         userID = 1;
-        document.getElementByID("message").innerHTML = "Welcome " + username + "#" + userID;
+        document.getElementByID("greeting").innerHTML = "Welcome " + username + "#" + userID;
     }
 }
 //please do not change this...
