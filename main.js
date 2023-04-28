891bfc3de5dee035e7e48f29698c50062eeed2b4
MKGames <120166433+iammkmusic@users.noreply.github.com>
2023-04-28 18:41:47 +0530

    if this doesn't work, i will quit making usernames

--- main.js
+++ main.js
@@ -4,22 +4,10 @@ while (userID!=5) {
 }
 function loginID() {
     var username = prompt("Enter your username", "example");
-    if (username==null) {
-        alert("Please enter a username");
-        return 0;
-    }
-    if (username==false) {
-        alert("Exiting manager...");
-        return 0;
-    }
-    if (username==true) {
-        document.getElementByID('message').innerHTML = "Welcome " + username;
-        return 0;
-        if (username=="ADMINPASS") {
-            userID = 1;
-            document.getElementByID("message").innerHTML = "Welcome " + username + "#" + userID;
-            return 0;
-        }
+    document.getElementByID('message').innerHTML = "Welcome " + username;
+    if (username=="ADMINPASS") {
+        userID = 1;
+        document.getElementByID("message").innerHTML = "Welcome " + username + "#" + userID;
     }
 }
 //please do not change this...
