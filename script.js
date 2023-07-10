1b8dbd600eaa9786ce027e5196f6ab7f40e3044a
znatgd <120166433+ZnatGD@users.noreply.github.com>
2023-07-10 17:03:56 +0530

    link improvements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hidden bugs...");
+/**
+ * Hello, this is my website.
+ * I should really make a readme file, but anyways
+ * You can read this on console too
+ */
+console.log("Hello, this is my website.");
+console.log("I should really make a readme file, but anyways");
+console.log("You are reading this on console (unless you are seeing the source)");
