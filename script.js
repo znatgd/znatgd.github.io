b557431c52c0ca28e9dc0e856c5bbc5901ee0bec
znatgd <120166433+ZnatGD@users.noreply.github.com>
2023-07-13 18:48:01 +0530

    surprise, surprise...
    I think this has gotten better with practise

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /**
  * Hello, this is my website.
- * I should really make a readme file, but anyways
+ * I should really make a readme file, but I'm too lazy LOL
  * You can read this on console too
  */
 console.log("Hello, this is my website.");
 console.log("I should really make a readme file, but anyways");
 console.log("You are reading this on console (unless you are seeing the source)");
+// plan for mobile friendly website
+var getWidth = window.innerWidth;
+var getHeight = window.innerHeight;
+console.log(getWidth);
+console.log(getHeight);
