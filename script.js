61c93f06f2da59d0d13218481b6af123aad09052
znatgd <120166433+ZnatGD@users.noreply.github.com>
2023-09-03 14:34:35 +0530

    New changes to website.

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-/**
- * Hello, this is my website.
- * I should really make a readme file, but I'm too lazy LOL
- * You can read this on console too
- */
-console.log("Hello, this is my website.");
-console.log("I should really make a readme file, but anyways");
-console.log("You are reading this on console (unless you are seeing the source)");
-var getWidth = window.innerWidth;
-var getHeight = window.innerHeight;
-console.log(getWidth);
-console.log(getHeight);
+var getWidth, getHeight;
 while (true) {
+    getWidth = window.innerWidth;
+    getHeight = window.innerHeight;
     if (getWidth < getHeight) {
         document.getElementsByTagName("body").style.fontSize = "x-large";
-        document.getElementsByClassName("headCont").style.fontSize = "x-small";
+        document.getElementsByClassName("headCont").style.fontSize = "x-large";
     }
 }
