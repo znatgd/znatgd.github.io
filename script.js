106cd08f06f8f1dd0584a5c0679aba24b119e6b3
znatgd <120166433+ZnatGD@users.noreply.github.com>
2023-09-16 18:33:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var getWidth = 0, getHeight = 0;
-document.getElementsByTagName("body").style.fontSize = "medium";
-document.getElementsByClassName("headCont").style.fontSize = "medium";
-
-function getDeviceType() {
-    getWidth = window.innerWidth;
-    getHeight = window.innerHeight;
-    if (getWidth < getHeight) {
-        document.getElementsByTagName("body").style.fontSize = "x-large";
-        document.getElementsByClassName("headCont").style.fontSize = "x-large";
-    } else {
-        document.getElementsByTagName("body").style.fontSize = "medium";
-        document.getElementsByClassName("headCont").style.fontSize = "medium";
-    }
-}
+//Edits
