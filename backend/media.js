0f1d93af3f21d84eecc355fdebf77fc6d7a650a5
Znatley <120166433+znatgd@users.noreply.github.com>
2025-06-25 21:07:23 +0530

    audio revamp

--- backend/media.js
+++ backend/media.js
@@ -1,13 +1,29 @@
-const song1 = { path: "https://znatgd.github.io/backend/FutureAdventures.mp3", songTitle: "Future Adventures", artistTitle: "ZnatGD", genre: "Genre: Future Funk"};
-const song2 = { path: "https://znatgd.github.io/backend/LOOP20.m4a", songTitle: "LOOP20", artistTitle: "ZnatGD", genre: "Genre: Freestyle"};
+const song1 = { path: "https://znatgd.github.io/backend/FutureAdventures.mp3", songTitle: "Future Adventures", artistTitle: "ZnatGD", genre: "Genre: Future Funk", duration: "04:44"};
+const song2 = { path: "https://znatgd.github.io/backend/LOOP20.m4a", songTitle: "LOOP20", artistTitle: "ZnatGD", genre: "Genre: Freestyle", duration: "01:27"};
 
 let currentIndex = -1;
 const maxSongs = 2;
 let isPlaying = false;
+let durationCurrent = "00:00";
 
 const audioPlayer = document.getElementById("player");
 
 window.alert("Please select a song before playing audio...");
+
+// Function to track and display the current position
+function trackAudioPosition() {
+    let currentTimeHere = document.getElementById("player").currentTime.toFixed(0);
+    formatData = ("0" + parseInt(Math.floor(currentTimeHere / 60))).slice(-2) + ":" +  ("0" + parseInt(Math.floor(currentTimeHere % 60))).slice(-2)
+    if (audioPlayer) {
+        document.getElementById("trackPos").innerText = (`Current Time: ${formatData} / ${durationCurrent}`);
+    }
+}
+
+// Add an event listener to update the position as the audio plays
+if (audioPlayer) {
+    document.getElementById("player").addEventListener("timeupdate", trackAudioPosition);
+}
+
 
 function previous() {
     audioPlayer.pause();
@@ -59,12 +75,14 @@ function switchSong(index) {
         document.getElementById("artistTitle").innerText = song1.artistTitle;
         document.getElementById("genre").innerText = song1.genre;
         document.getElementById("player").src = song1.path;
+        durationCurrent = song1.duration;
     } else if (index == 1) {
         document.body.style.backgroundImage = "url('https://znatgd.github.io/src/bg.png')";
         document.getElementById("songTitle").innerText = song2.songTitle;
         document.getElementById("artistTitle").innerText = song2.artistTitle;
         document.getElementById("genre").innerText = song2.genre;
         document.getElementById("player").src = song2.path;
+        durationCurrent = song2.duration;
     } else {
         document.body.style.backgroundImage = "url('https://znatgd.github.io/src/bg.png')";
         document.getElementById("songTitle").innerText = "noSuchSong";
